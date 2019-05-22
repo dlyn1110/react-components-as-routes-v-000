8a4302b1d4f27f9cf34a8cc0fd3f1b7ccbc7bacd
Dani Lyn <dlyn1110@gmail.com>
2019-05-22 23:17:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ const Login = () => {
 // Step 2. Changed to have router coordinate what is displayed
 ReactDOM.render((
   <Router>
+   <React.Fragment>
     <Route path="/" render={Home} />
     <Route exact path="/about" render={About} />
     <Route exact path="/login" render={Login} />
